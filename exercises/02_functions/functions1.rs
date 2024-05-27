d93a35156654b8e4302b918157cb8add0c9b0020
AbyssalRemark <dominicsevere@gmail.com>
2024-05-27 11:20:45 -0700

    need to migrate to pc

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,14 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
-fn main() {
+fn call_me()
+{
+    print!("Curly braces are hell.");
+}
+
+fn main() 
+{
     call_me();
 }
